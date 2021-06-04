9096af5d0513f4e6d843f3e916619feebc2acc47
Liam Bontkes <liambontkes1@gmail.com>
2021-06-03 21:10:15 -0700

    Added breakbeam functional description.

--- src/firmware/boards/robot_stm32h7/io/chicker.c
+++ src/firmware/boards/robot_stm32h7/io/chicker.c
@@ -12,6 +12,14 @@ void io_chicker_chip(float distance_m)
     // TODO (#2080) implement this
 }
 
+/**
+ * Breakbeam state should be "discovered" in two different ways:
+ *  1. whenever the breakbeam is broken, an interrupt should be triggered
+ *  2. create a function to poll the breakbeam GPIO pin for it's current status (for robot diagnostics)
+ * The interrupt should trigger a callback function when auto_chip and auto_kick are enabled
+ *
+ * @param speed_m_per_s
+ */
 void io_chicker_enable_auto_kick(float speed_m_per_s)
 {
     // TODO (#2080) implement this

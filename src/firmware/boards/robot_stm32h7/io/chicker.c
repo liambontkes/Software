8d323bc6c095a3482759f3d1c49ccd3c1bd49a67
Liam Bontkes <liambontkes1@gmail.com>
2021-06-03 21:52:58 -0700

    Added breakbeam struct and constructor.

--- src/firmware/boards/robot_stm32h7/io/chicker.c
+++ src/firmware/boards/robot_stm32h7/io/chicker.c
@@ -1,6 +1,31 @@
 #include "firmware/boards/robot_stm32h7/io/chicker.h"
-
 #include "firmware/app/logger/logger.h"
+
+#include <stdlib.h>
+
+typedef struct BreakBeam
+{
+    GpioPin_t* read_pin;
+} BreakBeam_t;
+
+/**
+ * Breakbeam state should be "discovered" in two different ways:
+ *  1. whenever the breakbeam is broken, an interrupt should be triggered
+ *  2. create a function to poll the breakbeam GPIO pin for it's current status (for robot diagnostics)
+ * The interrupt should trigger a callback function when auto_chip and auto_kick are enabled
+ *
+ * @return
+ */
+BreakBeam_t *io_break_beam_create(GpioPin_t *read_pin)
+{
+    BreakBeam_t* break_beam = (BreakBeam_t*)malloc(sizeof(BreakBeam_t));
+
+    break_beam->read_pin = read_pin;
+
+    io_gpio_pin_setActive(break_beam->read_pin);
+
+    return break_beam;
+}
 
 void io_chicker_kick(float speed_m_per_s)
 {
@@ -12,14 +37,6 @@ void io_chicker_chip(float distance_m)
     // TODO (#2080) implement this
 }
 
-/**
- * Breakbeam state should be "discovered" in two different ways:
- *  1. whenever the breakbeam is broken, an interrupt should be triggered
- *  2. create a function to poll the breakbeam GPIO pin for it's current status (for robot diagnostics)
- * The interrupt should trigger a callback function when auto_chip and auto_kick are enabled
- *
- * @param speed_m_per_s
- */
 void io_chicker_enable_auto_kick(float speed_m_per_s)
 {
     // TODO (#2080) implement this
@@ -38,4 +55,10 @@ void io_chicker_disable_auto_kick()
 void io_chicker_disable_auto_chip()
 {
     // TODO (#2080) implement this
+}
+
+bool io_get_breakbeam()
+{
+
+    return 0;
 }
--- src/firmware/boards/robot_stm32h7/io/chicker.h
+++ src/firmware/boards/robot_stm32h7/io/chicker.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <stdbool.h>
+
+#include "firmware/boards/robot_stm32h7/io/gpio_pin.h"
+
+typedef struct BreakBeam BreakBeam_t;
+
+BreakBeam_t* io_break_beam_create(GpioPin_t* read_pin);
+
 void io_chicker_kick(float speed_m_per_s);
 void io_chicker_chip(float distance_m);
 void io_chicker_enable_auto_kick(float speed_m_per_s);
 void io_chicker_enable_auto_chip(float distance_m);
 void io_chicker_disable_auto_kick();
 void io_chicker_disable_auto_chip();
+bool io_get_breakbeam();

ae11ff78ad0f1809c3cad85bba80af1f3e5ac717
Liam Bontkes <liambontkes1@gmail.com>
2021-06-12 11:41:16 -0700

    Added get breakbeam function.

--- src/firmware/boards/robot_stm32h7/io/chicker.h
+++ src/firmware/boards/robot_stm32h7/io/chicker.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include "firmware/boards/robot_stm32h7/io/gpio_pin.h"
+#include <stdbool.h>
+
 void io_chicker_kick(float speed_m_per_s);
 void io_chicker_chip(float distance_m);
 void io_chicker_enable_auto_kick(float speed_m_per_s);
 void io_chicker_enable_auto_chip(float distance_m);
 void io_chicker_disable_auto_kick();
 void io_chicker_disable_auto_chip();
+bool io_breakbeam_get_state(GpioPin_t* breakbeam_pin);
